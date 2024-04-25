019a61f01db955bd4821d3a8c36b67cc5e5a611b
samukaforrati <samuelforrati.aluno@unipampa.edu.br>
2024-04-25 19:25:26 -0300

    atualizado para bootstrap 5.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
 // Empty JS for your own code to be here
+
+
+        document.getElementById("modal-795925").addEventListener("click", function() {
+            const progressBar = document.getElementById("level1");
+            progressBar.style.width = "25%"; // Define a largura para 25%
+            progressBar.innerHTML = "25%"; // Exibe o texto dentro da barra de progresso
+        });
