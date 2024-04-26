f8c95b7a27519d820c9cbdd2e00f4d9de48c1f6b
samukaforrati <samuelforrati.aluno@unipampa.edu.br>
2024-04-25 21:15:47 -0300

    Fase 1 - ajustes

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,15 @@
 // Empty JS for your own code to be here
 
 
-        document.getElementById("modal-795925").addEventListener("click", function() {
+        document.getElementById("modal-nivel").addEventListener("click", function() {
             const progressBar = document.getElementById("level1");
             progressBar.style.width = "25%"; // Define a largura para 25%
             progressBar.innerHTML = "25%"; // Exibe o texto dentro da barra de progresso
         });
+
+        const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
